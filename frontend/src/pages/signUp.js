83f603c5ad9e5a8774925b4e4f81eb85e8b8538b
bhanunadar <bhanugoban@cogoport.com>
2023-06-17 21:43:39 +0530

    Create signUp.js

---
+++ frontend/src/pages/signUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUpComp from '../components/signUp';
+
+const SignUp = () => {
+	return (
+		<div>
+			<SignUpComp />
+		</div>
+	);
+};
+
+export default SignUp;
